51616f63d84a92bc2f6a85b8511ec8ddd897b1fe
Matt Brandt <mbrandt@mozilla.com>
2016-03-02 17:57:44 -0700

    oauth

--- openspacesboard.py
+++ openspacesboard.py
@@ -1,7 +1,9 @@
 import sqlite3
 from contextlib import closing
 
-from flask import Flask, g, jsonify, request, make_response
+
+from flask import Flask, g, jsonify, request, make_response, redirect, url_for, session
+from flask_oauthlib.client import OAuth
 
 # Configuration variables
 DATABASE = 'C:\\tmp\\openspacesboard.db'
@@ -27,6 +29,56 @@ class InvalidUsage(Exception):
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+
+oauth = OAuth(app)
+github = oauth.remote_app(
+    'github',
+    consumer_key='2a13319f2059625dbeb9',
+    consumer_secret='660136b970b0acc7b764c665cf3cea4b3ae332fc',
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+
+@app.route('/')
+def index():
+    if 'github_token' in session:
+        me = github.get('user')
+        return jsonify(me.data)
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error'],
+            request.args['error_description']
+        )
+    session['github_token'] = (resp['access_token'], '')
+    me = github.get('user')
+    return jsonify(me.data)
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
 
 
 @app.before_request
@@ -169,6 +221,6 @@ def delete_session(session_id):
     else:
         raise InvalidUsage('Session with ID: {} does not exist'.format(session_id), status_code=400)
 
+
 if __name__ == '__main__':
     app.run()
-
