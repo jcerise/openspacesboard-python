a0b3f8d81aa008083c11652f40bb6fa171b6d0da
Jeremy Cerise <jcerise06@gmail.com>
2016-03-05 18:50:58 -0700

    Setting run script to default to 8080 if no port has been defined.

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+import os
+
 from osbp_app import app
 
-app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
